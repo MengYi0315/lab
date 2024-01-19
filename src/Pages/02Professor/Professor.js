1207d10130c17714b9e4f59212150ace9caf5acb
MengYi0315 <mindy.1030529@gmail.com>
2024-01-19 17:25:34 +0800

    update professor

--- src/Pages/02Professor/Professor.js
+++ src/Pages/02Professor/Professor.js
@@ -1,11 +1,14 @@
-import { Flex } from "@mantine/core";
+import { Flex, Image, Grid } from "@mantine/core";
 
 const Professor = (props) => {
     return (
         <div id="Professor">
 
             <Flex>
-                {/* <Image /> */}
+                <Image
+                    src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-10.png"
+                />
+                <Flex></Flex>
             </Flex>
         </div>
     )
