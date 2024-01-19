5044b9ae31d070fbdebb257ae1bb6243ef4b516e
MengYi0315 <mindy.1030529@gmail.com>
2024-01-19 18:32:31 +0800

    update login

--- src/Pages/Auth/Login.js
+++ src/Pages/Auth/Login.js
@@ -1,7 +1,56 @@
+import { Input, Button, Center, Image, Space, Stack, Card } from "@mantine/core";
+import "../Auth/Login.scss";
+import logo from "../../assets/logo.png";
+
+
 const Login = (props) => {
     return (
-        <div>
-            auth login
+        <div id="Login">
+            <Center>
+                <Image 
+                    src={logo}
+                    h={100}
+                    w="auto"
+                    className="logo"
+                />
+
+            </Center>
+            <Center>
+            <Card
+                shadow="sm"
+                padding={"lg"}
+                radius={"md"}
+                withBorder
+                style={{width:'50%', marginTop:'10px'}}
+            >
+                {/* <Card.Section>
+                    
+                </Card.Section> */}
+                <Space h="xl" />
+                
+                <Stack
+                    align="center"
+                    justify="flex-start"
+                    gap={"md"}
+                >
+                    <Input
+                        placeholder="enter account..."
+                        style={{width:'50%'}}
+                    />
+                    <Input
+                        placeholder="enter password..."
+                        style={{width:'50%'}}
+                    />
+                    <Button size="md" mt="md" radius="md" style={{width:'50%'}}>
+                        Login
+                    </Button>
+                </Stack>
+                
+
+            </Card>
+            </Center>
+
+            
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { MantineProvider } from '@mantine/core';
+import { MantineProvider, createTheme } from '@mantine/core';
 
 import './index.css';
 import App from './App';
+
+const theme = createTheme({
+  fontFamily: '"Noto Sans TC", sans-serif', 
+  primaryColor: 'indigo'
+});
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MantineProvider>
+    <MantineProvider theme={theme}>
       <App />
     </MantineProvider>
   </React.StrictMode>
