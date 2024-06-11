55527b3fb26805dd4c859ff50be99e1a72fd0cde
MengYi0315 <mindy.1030529@gmail.com>
2024-06-11 17:46:24 +0800

    update

--- src/Router.js
+++ src/Router.js
@@ -54,5 +54,5 @@ const router = createBrowserRouter([
 
 
 export default function App() {
-  return <RouterProvider router={router} />;
+  return <RouterProvider router={router} basename="/lab" />;
 }
